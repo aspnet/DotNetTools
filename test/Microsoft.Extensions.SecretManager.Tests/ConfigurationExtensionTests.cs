9a4b2fa445153fb10cbd5ce5508070b62313c0f8
John Luo <jt.luo@mail.utoronto.ca>
2017-02-12 16:21:03 -0800

    Remove redundant test

--- test/Microsoft.Extensions.SecretManager.Tests/ConfigurationExtensionTests.cs
+++ test/Microsoft.Extensions.SecretManager.Tests/ConfigurationExtensionTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using Microsoft.AspNetCore.Testing.xunit;
 using Microsoft.Extensions.SecretManager.Tests;
 using Microsoft.Extensions.SecretManager.Tools;
 using Xunit;
@@ -17,37 +16,6 @@ namespace Microsoft.Extensions.Configuration.UserSecrets.Tests
         {
             var builder = new ConfigurationBuilder()
                                 .AddUserSecrets(userSecretsId: Guid.NewGuid().ToString());
-        }
-
-        [ConditionalFact (Skip = "Obsolete API")]
-        public void AddUserSecrets_Does_Not_Fail_On_Non_Existing_File()
-        {
-            var projectPath = UserSecretHelper.GetTempSecretProject();
-
-            var builder = new ConfigurationBuilder().SetBasePath(projectPath).AddUserSecrets();
-            var configuration = builder.Build();
-            Assert.Equal(null, configuration["Facebook:AppSecret"]);
-
-            UserSecretHelper.DeleteTempSecretProject(projectPath);
-        }
-
-        [ConditionalFact (Skip = "Obsolete API")]
-        public void AddUserSecrets_With_An_Existing_Secret_File()
-        {
-            string userSecretsId;
-            var projectPath = UserSecretHelper.GetTempSecretProject(out userSecretsId);
-
-            var logger = new TestLogger();
-            var secretManager = new Program() { Logger = logger };
-
-            secretManager.Run(new string[] { "set", "Facebook:AppSecret", "value1", "-p", projectPath });
-
-            var builder = new ConfigurationBuilder().SetBasePath(projectPath).AddUserSecrets();
-
-            var configuration = builder.Build();
-            Assert.Equal("value1", configuration["Facebook:AppSecret"]);
-
-            UserSecretHelper.DeleteTempSecretProject(projectPath);
         }
 
         [Fact]
